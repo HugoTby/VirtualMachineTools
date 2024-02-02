9c771259f50eee75a60cfa7b093c673bad47bb0a
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2024-02-02 14:39:50 +0100

    Update debian_all_programs.sh

--- debian_all_programs.sh
+++ debian_all_programs.sh
@@ -1,8 +1,8 @@
-apt-get -y update
+apt-get update
 apt-get -y install apache2
 apt-get -y install mariadb-server
 apt-get -y install php7.4 php-mysql php-xml
-apt-get -y update
+apt-get update
 apt-get -y install wget
 cd /var/www/html
 wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.tar.gz
@@ -12,4 +12,4 @@ mv phpMyAdmin-5.2.1-all-languages/ phpmyadmin
 apt-get -y install samba samba-common-bin
 cd /etc/samba
 cp /etc/samba/smb.conf /etc/samba/smb.conf.old
-apt-get -y update
+apt-get update
