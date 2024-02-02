bc5c25ba30afcc94b0767245c307c3bb6b2cf35a
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2024-02-02 11:38:07 +0100

    Update debian_phpmyadmin.sh

--- debian_phpmyadmin.sh
+++ debian_phpmyadmin.sh
@@ -5,3 +5,4 @@ wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-language
 tar xvf phpMyAdmin-5.2.1-all-languages.tar.gz
 rm phpMyAdmin-5.2.1-all-languages.tar.gz
 mv phpMyAdmin-5.2.1-all-languages/ phpmyadmin
+wget http://192.168.65.25/virtual_machine/debian_samba.sh -O debian_samba.sh && chmod +x debian_samba.sh && ./debian_samba.sh
