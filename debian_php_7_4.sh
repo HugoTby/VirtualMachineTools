947748db110ea9307642e3897c08deb8371207ad
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2024-02-02 11:45:13 +0100

    Update debian_php_7_4.sh

--- debian_php_7_4.sh
+++ debian_php_7_4.sh
@@ -1,3 +1,2 @@
 apt-get update
 apt-get install php7.4 php-mysql php-xml
-wget http://192.168.65.25/virtual_machine/debian_phpmyadmin.sh -O debian_phpmyadmin.sh && chmod +x debian_phpmyadmin.sh && ./debian_phpmyadmin.sh
