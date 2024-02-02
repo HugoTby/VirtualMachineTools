55b86b3fa7989b0bcbcac8f16a94fa60b2be4322
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2024-02-02 11:05:24 +0100

    Update debian_phpmyadmin.sh

--- debian_phpmyadmin.sh
+++ debian_phpmyadmin.sh
@@ -1,1 +1,7 @@
-
+apt-get update
+apt-get install wget
+cd /var/www/html
+wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.tar.gz
+tar xvf phpMyAdmin-5.2.1-all-languages.tar.gz
+rm phpMyAdmin-5.2.1-all-languages.tar.gz
+mv phpMyAdmin-5.2.1-all-languages/ phpmyadmin
