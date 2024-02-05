c879f6ac5167fd68f815300de04004e44fa49ba0
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2024-02-05 17:07:39 +0100

    Update debian_all_programs.sh

--- debian_all_programs.sh
+++ debian_all_programs.sh
@@ -18,13 +18,14 @@ wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-language
 tar xvf phpMyAdmin-5.2.1-all-languages.tar.gz
 rm phpMyAdmin-5.2.1-all-languages.tar.gz
 mv phpMyAdmin-5.2.1-all-languages/ phpmyadmin
-echo "root"
+
 mysql -u root -p <<EOF
 SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root');
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 quit
 EOF
+
 cd /etc/mysql/mariadb.conf.d
 cp /etc/mysql/mariadb.conf.d/50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf.old
 wget -O /etc/mysql/mariadb.conf.d/50-server.cnf  http://192.168.65.25/virtual_machine/50-server.cnf
