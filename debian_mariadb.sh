0bb6af20cd93d4315efc0910dabfd751da8629b6
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2024-02-05 13:07:26 +0100

    Update debian_mariadb.sh

--- debian_mariadb.sh
+++ debian_mariadb.sh
@@ -1,2 +1,13 @@
 apt-get update
-apt-get install mariadb-server
+apt-get install mariadb-server -y
+
+# Automatisation de la configuration sécurisée de MariaDB
+echo "root" | mysql_secure_installation <<EOF
+
+y
+n
+n
+y
+y
+y
+EOF
