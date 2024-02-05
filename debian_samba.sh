b64d3f402366a0649e346fd8989267fb19b819ba
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2024-02-05 13:03:06 +0100

    Update debian_samba.sh

--- debian_samba.sh
+++ debian_samba.sh
@@ -4,6 +4,4 @@ cd /etc/samba
 cp /etc/samba/smb.conf /etc/samba/smb.conf.old
 wget -O /etc/samba/smb.conf http://192.168.65.25/virtual_machine/smb.conf
 service smbd restart
-smbpasswd -a root
-root
-root
+(echo "root"; sleep 1; echo "root") | smbpasswd -a root
