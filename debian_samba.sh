5e3f944ebf74b7dd6b46728e369fa2597d25c90a
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2024-02-05 13:28:29 +0100

    Update debian_samba.sh

--- debian_samba.sh
+++ debian_samba.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install samba samba-common-bin
+apt-get -y install samba samba-common-bin
 cd /etc/samba
 cp /etc/samba/smb.conf /etc/samba/smb.conf.old
 wget -O /etc/samba/smb.conf http://192.168.65.25/virtual_machine/smb.conf
