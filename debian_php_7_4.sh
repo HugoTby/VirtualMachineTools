d609f3a2360058668ad7e51b0f0476514494e510
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2024-02-05 13:11:32 +0100

    Update debian_php_7_4.sh

--- debian_php_7_4.sh
+++ debian_php_7_4.sh
@@ -1,2 +1,6 @@
 apt-get update
 apt-get install php7.4 php-mysql php-xml
+cd /var/www/html
+rm index.html
+wget -O /var/www/html/index.php http://192.168.65.25/virtual_machine/index.php
+service apache2 restart
