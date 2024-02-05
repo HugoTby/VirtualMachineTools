7ba2e8757fc029ff36cb9d18182aa369a621f7f3
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2024-02-05 17:09:28 +0100

    Update debian_all_programs.sh

--- debian_all_programs.sh
+++ debian_all_programs.sh
@@ -9,11 +9,14 @@ y
 y
 y
 EOF
+
 apt-get -y install php7.4 php-mysql php-xml
 cd /var/www/html
 rm index.html
 wget -O /var/www/html/index.php http://192.168.65.25/virtual_machine/index.php
 service apache2 restart
+
+cd /var/www/html
 wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.tar.gz
 tar xvf phpMyAdmin-5.2.1-all-languages.tar.gz
 rm phpMyAdmin-5.2.1-all-languages.tar.gz
@@ -25,11 +28,11 @@ GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRAN
 FLUSH PRIVILEGES;
 quit
 EOF
-
 cd /etc/mysql/mariadb.conf.d
 cp /etc/mysql/mariadb.conf.d/50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf.old
 wget -O /etc/mysql/mariadb.conf.d/50-server.cnf  http://192.168.65.25/virtual_machine/50-server.cnf
 service mariadb restart
+
 apt-get -y install samba samba-common-bin
 cd /etc/samba
 cp /etc/samba/smb.conf /etc/samba/smb.conf.old
