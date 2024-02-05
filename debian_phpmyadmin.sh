d43dece36dcd23ee1135cdbd08ae89fca4be56fa
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2024-02-05 13:33:20 +0100

    Update debian_phpmyadmin.sh

--- debian_phpmyadmin.sh
+++ debian_phpmyadmin.sh
@@ -4,3 +4,11 @@ wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-language
 tar xvf phpMyAdmin-5.2.1-all-languages.tar.gz
 rm phpMyAdmin-5.2.1-all-languages.tar.gz
 mv phpMyAdmin-5.2.1-all-languages/ phpmyadmin
+
+# Connexion à MySQL pour exécuter les commandes SQL
+mysql -u root -p <<EOF
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root');
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+quit
+EOF
